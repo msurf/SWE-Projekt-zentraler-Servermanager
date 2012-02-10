5ef574e18262cdcb638314a2949d342c4e2ea7ae
Philipp Tendyra <p.tendyra@gmail.com>
2012-02-10 09:36:41 +0100

    Ausführungsrechte aktualisiert und beschrieben, siehe KOmmentare im Script

--- clientdaemon/createuser.sh
+++ clientdaemon/createuser.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 ## Usage: createuser USERNAME PASSWORD
+
+## The User who runs this script needs adminrights
+## so add the following two lines to visudo
+## %USER ALL=NOPASSWD: way-to-createuser.sh
+## %USER ALL=NOPASSWD: /usr/sbin/useradd
+
+## if you can't run visudo please check if sudo is installed, else get it
+
 ## by Philipp Tendyra
 
 
@@ -10,10 +18,16 @@ then
 	passwort=$2
 	salt=$(($RANDOM % 100))
 	crypt=$(perl -e 'print crypt("'$passwort'", "'$salt'")')
-	useradd -m -s /bin/bash -p $crypt $1
+	sudo useradd -m -s /bin/bash -p $crypt $1
 	if [ $? = 0 ]
 	then
-		echo The User $1 was created successfully
+		grep $1 /etc/passwd
+		if [ $? = 0 ]
+		then
+			echo The User $1 was created successfully
+		else
+			echo a problem accrued. please check.
+		fi
 	else
 		echo User could not be added
 	fi
