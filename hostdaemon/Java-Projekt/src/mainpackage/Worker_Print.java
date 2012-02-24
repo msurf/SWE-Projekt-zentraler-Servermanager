5ee3e42dba952d2c01cb0a880d79f62f9eeac96a
Philipp Tendyra <p.tendyra@gmail.com>
2012-02-24 18:02:08 +0100

    Hostprogramm mit TODOs im ServerSocketThread versehen

---
+++ hostdaemon/Java-Projekt/src/mainpackage/Worker_Print.java
@@ -0,0 +1,25 @@
+package mainpackage;
+
+public class Worker_Print extends Worker{
+	
+	Worker_Print(Command command, Communication com, Config conf) {
+		super(command, com, conf);
+	}
+	
+	public void run() {
+		printdef();
+		//print(this._command.clone());
+		//log(this._message);
+	}//run()
+	
+	private void printdef(){System.out.println("working on Command: "+this._command.getName());}
+	
+	private void print(Command c){
+		System.out.println("Name -> " + c.getName());
+		System.out.println("From -> " + c.getFrom());
+		System.out.println("Parameters -> " + c.getParameter());
+		System.out.println("Direction -> " + c.getDirection());
+		System.out.println("FTP URL -> " + c.getFTP_URL());
+		System.out.println("Query -> " + c.getQuery());
+	}//print()
+}
