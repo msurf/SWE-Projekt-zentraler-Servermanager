a543095e42576945d5eb7c73ef85373208791e5e
Philipp Tendyra <p.tendyra@gmail.com>
2012-02-27 12:13:41 +0100

    Worker_addclient hinzugefügt

--- hostdaemon/Java-Projekt/src/mainpackage/Worker_addclient.java
+++ hostdaemon/Java-Projekt/src/mainpackage/Worker_addclient.java
@@ -1,5 +1,15 @@
 package mainpackage;
 
-public class Worker_addclient {
+public class Worker_addclient extends Worker{
+	
+	public Worker_addclient(Command command, Config conf, Communication com) {
+		super();
+		this._command = command;
+		this._conf = conf;
+		this._com = com;
+	}
+	public void run(){
+		
+	}
 
 }
