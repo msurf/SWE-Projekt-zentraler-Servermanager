7c0c9d924a6816d8a186600537a0f9df4bb0cf09
Tobias Thieron <tthieron@hotmail.de>
2012-02-29 23:11:04 +0100

    Worker_install wurde bearbeitet

--- hostdaemon/Java-Projekt/src/mainpackage/Worker_install.java
+++ hostdaemon/Java-Projekt/src/mainpackage/Worker_install.java
@@ -1,5 +1,7 @@
 package mainpackage;
 
+import java.sql.SQLException;
+
 public class Worker_install extends Worker{
 
 	public Worker_install(Command command, Config conf, Communication com) {
@@ -9,6 +11,42 @@ public class Worker_install extends Worker{
 		this._com = com;
 	}
 	public void run(){
-		
+		sendToClient();
+	}
+	
+	public void sendToClient(){
+		Database data = new Database();
+		String url="";
+		try {
+			url = data.getClientIP(this._command.getClientID());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String ip = url.split(":")[0];
+		try {
+			this._command.setFTP_IP(data.getFTP_IP_FILE(Integer.parseInt(this._command.getParameter()))[0]);
+			this._command.setFTP_File(data.getFTP_IP_FILE(Integer.parseInt(this._command.getParameter()))[1]);
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		int port = Integer.parseInt(url.split(":")[1]);
+		this._com.send(this._command, ip, port);
 	}
 }
