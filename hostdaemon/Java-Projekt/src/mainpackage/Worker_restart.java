08c223dbcd8380727dd67ea68dd02a20ddf2cdae
Tobias Thieron <tthieron@hotmail.de>
2012-02-29 23:45:59 +0100

    Worker geaendert

--- hostdaemon/Java-Projekt/src/mainpackage/Worker_restart.java
+++ hostdaemon/Java-Projekt/src/mainpackage/Worker_restart.java
@@ -1,5 +1,7 @@
 package mainpackage;
 
+import java.sql.SQLException;
+
 public class Worker_restart extends Worker{
 	
 	public Worker_restart(Command command, Config conf, Communication com) {
@@ -9,7 +11,29 @@ public class Worker_restart extends Worker{
 		this._com = com;
 	}
 	public void run(){
+		sendToClient();
+	}
+	
+	public void sendToClient(){
+		Database data = new Database();
+		String url="";
+		try {
+			url = data.getClientIP(this._command.getClientID());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String ip = url.split(":")[0];
 		
+		int port = Integer.parseInt(url.split(":")[1]);
+	
+		this._com.send(this._command, ip, port);
 	}
 
 }
--- hostdaemon/Java-Projekt/src/mainpackage/Worker_start.java
+++ hostdaemon/Java-Projekt/src/mainpackage/Worker_start.java
@@ -1,5 +1,7 @@
 package mainpackage;
 
+import java.sql.SQLException;
+
 public class Worker_start extends Worker{
 
 	public Worker_start(Command command, Config conf, Communication com) {
@@ -9,6 +11,28 @@ public class Worker_start extends Worker{
 		this._com = com;
 	}
 	public void run(){
+		sendToClient();
+	}
+	
+	public void sendToClient(){
+		Database data = new Database();
+		String url="";
+		try {
+			url = data.getClientIP(this._command.getClientID());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String ip = url.split(":")[0];
 		
+		int port = Integer.parseInt(url.split(":")[1]);
+	
+		this._com.send(this._command, ip, port);
 	}
 }
