cd61666fce5901427932f1677a6787d47c9b38f1
Philipp Tendyra <p.tendyra@gmail.com>
2012-03-01 16:28:10 +0100

    update

--- hostdaemon/Java-Projekt/src/mainpackage/Database.java
+++ hostdaemon/Java-Projekt/src/mainpackage/Database.java
@@ -8,6 +8,7 @@ public class Database {
 	}
 	
 	public void deleteDB() throws ClassNotFoundException, SQLException{
+		System.out.println("Start deleting Tables ->");
 		Class.forName("org.sqlite.JDBC");
 		Connection conn = DriverManager.getConnection("jdbc:sqlite:servermanager.db");
 		Statement stat = conn.createStatement();
@@ -16,45 +17,48 @@ public class Database {
 		stat.executeUpdate("drop table if exists software");
 		stat.executeUpdate("drop table if exists installierte_software");
 		stat.executeUpdate("drop table if exists hardware");
-		System.out.println("All Tables deleted");
+		System.out.println("<- All Tables deleted");
 	}
 	
 	protected void create_DB() throws ClassNotFoundException, SQLException, Exception {
+			System.out.println("Start creating Tables ->");
 			Class.forName("org.sqlite.JDBC");
 			Connection conn = DriverManager.getConnection("jdbc:sqlite:servermanager.db");
 			Statement stat = conn.createStatement();
-			stat.executeUpdate("create table if !exists Benutzer( Name text primary key not null unique,"+
+			stat.executeUpdate("create table if not exists Benutzer( Name text primary key not null unique,"+
 										  										  "Pwd text not null,"+
 										  										  "Rights text not null,"+
 										  										  "check (Rights = 'admin' or Rights = 'read'));");
-			stat.executeUpdate("create table if !exists Client( Name text not null unique,"+
+			stat.executeUpdate("create table if not exists Client( Name text not null unique,"+
 																			 "IP text not null unique,"+
 																			 "Client_ID integer primary key autoincrement not null unique,"+
 																			 "user text not null default 'root',"+
 																			 "pw text not null default 'swe1234',"+
 																			 "status text not null default 'off',"+
 																			 "check (status = 'busy' or status = 'off' or status = 'on'));");
-			stat.executeUpdate("create table if !exists Software( Software_ID integer primary key autoincrement not null unique,"+
+			stat.executeUpdate("create table if not exists Software( Software_ID integer primary key autoincrement not null unique,"+
 																				  "Name  text not null unique,"+
 																				  "Beschreibung text,"+
 																				  "FTP_Pfad text not null,"+
-																				  "FTP_IP text not null;");
-			stat.executeUpdate("create table if !exists Installierte_Software(ID integer primary key autoincrement not null unique,"+
+																				  "FTP_IP text not null);");
+			stat.executeUpdate("create table if not exists Installierte_Software(ID integer primary key autoincrement not null unique,"+
 											 														 "Software_ID references Software(Software_ID) on delete restrict on update restrict not null,"+
 											 														 "Client_ID references Client(Client_ID) on delete restrict on update restrict not null,"+
 											 														 "Software_Benutzer text,"+
 											 														 "Passwort text,"+
 											 														 "Status text not null default 'install',"+
 											 														 "check (status='install' or status = 'on' or status = 'off'));");
-			stat.executeUpdate("create table if !exists Hardware( Client_ID references Software(Software_ID) on delete restrict on update restrict primary key,"+
+			stat.executeUpdate("create table if not exists Hardware( Client_ID references Software(Software_ID) on delete restrict on update restrict primary key,"+
 																					"CPU text,"+
 																					"RAM text,"+
 																					"Architecture text);");
 			stat.close();
 			conn.close();
+			System.out.println("<- All Tables created");
 	}
 	
 	public void fillDB() throws ClassNotFoundException, SQLException{
+		System.out.println("Start creating Entries ->");
 		Class.forName("org.sqlite.JDBC");
 		Connection conn = DriverManager.getConnection("jdbc:sqlite:servermanager.db");
 		Statement stat = conn.createStatement();
@@ -69,30 +73,43 @@ public class Database {
 		stat.executeUpdate("insert into benutzer(name,pwd,rights) values ('fwolff','wolffpw','admin')");
 		
 		/* Clients */
-		stat.executeUpdate("insert into client(name,ip,user,pw) values ('debian2','192.168.21.22','debian2','debian2')");
-		stat.executeUpdate("insert into client(name,ip,user,pw) values ('debian3','192.168.21.23','debian3','debian3')");
+		stat.executeUpdate("insert into client(name,ip,user,pw) values ('debian2','192.168.21.22:5550','debian2','debian2')");
+		stat.executeUpdate("insert into client(name,ip,user,pw) values ('debian3','192.168.21.23:5550','debian3','debian3')");
 		/* Software */
-		//stat.executeUpdate("insert into software(name,beschreibung,ftp_pfad,ftp_ip) values ('css','Installiert CounterStrike:Source','css.tar.gz','192.168.1.21')");
+		stat.executeUpdate("insert into software(name,beschreibung,ftp_pfad,ftp_ip) values ('css','Installiert CounterStrike:Source','css.tar.gz','192.168.1.21')");
 		
 		/* installierte Software*/
-		/* 	
-		+	rs = stat.executeQuery("select software_id from software where name='css'");
-		+	while(rs.next())
-		+	softwareid = rs.getString("software_id");
-		+	rs = stat.executeQuery("select client_id from client where name='debian1'");
-		+	while(rs.next())
-		+		clientid = rs.getString("client_id");
-		+
-		+	stat.executeUpdate("insert into installierte_software(software_id,client_id,software_benutzer,passwort) values ('"+softwareid+"','"+clientid+"','css1','none')");
-		*/
+		 	
+			rs = stat.executeQuery("select software_id from software where name='css'");
+			while(rs.next())
+			softwareid = rs.getString("software_id");
+			rs = stat.executeQuery("select client_id from client where name='debian2'");
+			while(rs.next())
+				clientid = rs.getString("client_id");
 		
+			stat.executeUpdate("insert into installierte_software(software_id,client_id,software_benutzer,passwort) values ('"+softwareid+"','"+clientid+"','css1','none')");
+			stat.executeUpdate("insert into installierte_software(software_id,client_id,software_benutzer,passwort) values ('"+softwareid+"','"+clientid+"','css2','none')");
+			stat.executeUpdate("insert into installierte_software(software_id,client_id,software_benutzer,passwort) values ('"+softwareid+"','"+clientid+"','css3','none')");
+		
+			rs = stat.executeQuery("select client_id from client where name='debian3'");
+			while(rs.next())
+				clientid = rs.getString("client_id");	
+			stat.executeUpdate("insert into installierte_software(software_id,client_id,software_benutzer,passwort) values ('"+softwareid+"','"+clientid+"','css1','none')");
+			stat.executeUpdate("insert into installierte_software(software_id,client_id,software_benutzer,passwort) values ('"+softwareid+"','"+clientid+"','css2','none')");
+
 		/* Hardware */
-		rs = stat.executeQuery("select client_id from client where name='debian1'");
+		rs = stat.executeQuery("select client_id from client where name='debian2'");
 		while(rs.next())
 			clientid = rs.getString("client_id");
 		
 		stat.executeUpdate("insert into hardware(client_id,cpu,ram,architecture) values ('"+clientid+"','C2D@2.667Ghz','2024Mb','i386')");
+		rs = stat.executeQuery("select client_id from client where name='debian3'");
+		while(rs.next())
+			clientid = rs.getString("client_id");
+		stat.executeUpdate("insert into hardware(client_id,cpu,ram,architecture) values ('"+clientid+"','AMD@1.8Ghz','4096Mb','amd64')");
 		
+		
+		System.out.println("<- All Tables updated");
 	}
 	
 	protected String getInfo_Authenticate(String user, String password) throws SQLException, ClassNotFoundException, Exception {
@@ -150,9 +167,9 @@ public class Database {
 		Class.forName("org.sqlite.JDBC");
 		Connection conn = DriverManager.getConnection("jdbc:sqlite:servermanager.db");
 		Statement stat = conn.createStatement();
-		ResultSet rs = stat.executeQuery("select distinct Name from Software;");
+		ResultSet rs = stat.executeQuery("select distinct name from Software;");
 		while(rs.next()) {
-			ergebnis += "#"+rs.getString(1);
+			ergebnis += "#"+rs.getString("name");
 		}
 		ergebnis += ergebnis.replace("none#", "");
 		rs.close();
@@ -166,9 +183,9 @@ public class Database {
 		Class.forName("org.sqlite.JDBC");
 		Connection conn = DriverManager.getConnection("jdbc:sqlite:servermanager.db");
 		Statement stat = conn.createStatement();
-		ResultSet rs = stat.executeQuery("select CPU, RAM, Architecture from Hardware where Client_ID = "+clientID+";");
+		ResultSet rs = stat.executeQuery("select cpu,ram,architecture from Hardware where Client_ID = '"+clientID+"';");
 		while (rs.next()) {
-			ergebnis = "cpu:"+rs.getString(1)+"#ram:"+rs.getString(2)+"#architecture:"+rs.getString(3);
+			ergebnis = "cpu:"+rs.getString("cpu")+"#ram:"+rs.getString("ram")+"#architecture:"+rs.getString("architecture");
 		}
 		rs.close();
 		stat.close();
@@ -181,9 +198,9 @@ public class Database {
 		Class.forName("org.sqlite.JDBC");
 		Connection conn = DriverManager.getConnection("jdbc:sqlite:servermanager.db");
 		Statement stat = conn.createStatement();
-		ResultSet rs = stat.executeQuery("select Status, Software_Benutzer from Installierte_Software where Client_ID = "+clientID+";");
+		ResultSet rs = stat.executeQuery("select status, software_benutzer from Installierte_Software where Client_ID = '"+clientID+"';");
 		while (rs.next()) {
-			ergebnis += "#" + rs.getString(2)+":"+rs.getString(1);
+			ergebnis += "#" + rs.getString("software_benutzer")+":"+rs.getString("status");
 		}
 		ergebnis = ergebnis.replace("none#", "");
 		rs.close();
