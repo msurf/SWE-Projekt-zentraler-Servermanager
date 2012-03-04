08e38a7aecb1a5b6b2f52d37b38e603b388acb50
Philipp Tendyra <p.tendyra@gmail.com>
2012-03-04 22:24:26 +0100

    packagebug gefixt

--- hostdaemon/Commandtester/src/mainpackage/INIT.java
+++ hostdaemon/Commandtester/src/mainpackage/INIT.java
@@ -7,10 +7,6 @@ public class INIT {
 	public static void main(String[] args) {
 		String ip ="localhost";
 		int port = 5550;
-
-		ip = getText("Sending to IP=");
-		
-		
 		Scanner sc = new Scanner(System.in);
 		int input = -1;
 		while(input != 0){
@@ -22,29 +18,9 @@ public class INIT {
 				OutputThread out = new OutputThread(ip, port, c);
 				out.start();
 				input = -1;
-				
-				String conf = getText("Do you want to store Command?(y/n)");
-				String conf2 = conf.toLowerCase();
-				if(conf2.equals("y") || conf2.equals("yes"))
-				{
-					Packer p = new Packer(getText("Please insert Filename:"));
-					p.pack(c);
-				}
 			}
+			
 		}
 	}
-	private static String getText(String message){
-		Scanner sc = new Scanner(System.in);
-		try{
-			System.out.println(message);
-			String tmp = sc.nextLine();
-			if(tmp.length() > 0)
-				return tmp;
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-		return "default";
-	}	
+
 }
